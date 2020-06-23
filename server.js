6065ea1fb8a69d4091ea0cba4744bb213ff8c6c3
zunayed.ahmed@ieee.org <zunayed.ahmed@ieee.org>
2020-06-23 21:41:04 +0600

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-round-49590',
-    user : 'postgres',
-    password : '12345',
-    database : 'smart-brain'
+    host : 'process.env.DATABASE_URL',
+    ssl: true,
   }
 });
 
